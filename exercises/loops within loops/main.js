78487657f0945b4c4fc7a298fb632512bb4fd17b
V School Student <student@Vs-Mac-Pro-3.local>
2018-03-09 14:40:49 -0700

    Loops within Loops

---
+++ exercises/loops within loops/main.js
@@ -0,0 +1,11 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];  
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var myArr = []
+
+for (var i = 0; i < people.length; i++) {
+    myArr.push (people[i])
+    for (var j = 0; j < alphabet.length; j++) {
+        myArr.push (alphabet[j])
+    }
+}
+console.log(myArr);
